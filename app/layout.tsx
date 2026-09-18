67b035ad08263fda7edb44786dbc18d6cd1be998
reiiihuh <reimitsune@gmail.com>
2026-09-18 08:56:57 +0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
     "codex-preview": "development",
   },
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
   },
 };
 
